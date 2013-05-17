96614f3d2fbc82bae9b76e8c8f2edea2796afd23
TCAD <tcad@TCAD7.(none)>
2013-05-18 02:51:49 +0300

    issue1

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInitd0d13d482f6d226c96ef3a3b4f93e495::getLoader();
+return ComposerAutoloaderInit7000d69beebdf9c0821fb369f9524423::getLoader();
